88aef30e68ddddf9a61443288261836d0ad9cb5c
ishansaini194 <ishan.saini880@gmail.com>
2026-03-12 14:26:54 +0530

    speration added

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,27 +2,22 @@ package main
 
 import (
 	"log"
-	"os"
 
-	"github.com/ishansaini194/customurls/internal/server"
-	"github.com/ishansaini194/customurls/internal/shorturl"
+	"github.com/ishansaini194/customurls/config"
+	"github.com/ishansaini194/customurls/internal/app"
 )
 
 func main() {
-	httpServer := server.New()
-
-	dbURL := os.Getenv("DATABASE_URL")
-
-	repo, err := shorturl.NewPostgresRepository(dbURL)
+	cfg, err := config.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	service := shorturl.NewService(repo)
-	handler := shorturl.NewHandler(service)
+	srv, err := app.New(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	httpServer.App.Post("/shorten", handler.CreateShortUrl)
-	httpServer.App.Get("/:custom", handler.Redirect)
-
-	log.Fatal(httpServer.Start(":8080"))
+	log.Printf("server starting on :%s", cfg.Port)
+	log.Fatal(srv.Start(":" + cfg.Port))
 }
