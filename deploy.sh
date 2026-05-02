b7d15feba946bbd58729afd7a895c80b36cc58fc
Ishan saini <ishan.saini880@gmail.com>
2026-05-02 21:29:02 +0530

    added prune command

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ ssh root@168.144.116.143 "cd ~/customurls && git pull origin main"
 echo "🐳 Rebuilding Docker containers..."
 ssh root@168.144.116.143 "cd ~/customurls && docker-compose down -v && docker-compose up -d --build"
 
+echo "cleaning unused Docker resources..."
+ssh root@168.144.166.143 "docker system prune -f"
+
 echo "🔄 Checking container status..."
 ssh root@168.144.116.143 "docker ps"
 
